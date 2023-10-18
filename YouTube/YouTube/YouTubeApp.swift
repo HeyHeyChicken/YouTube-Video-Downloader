e24bfd407873194e3c1647fd56f0021cab37c64a
Antoine Duval <antoine@cuffel.fr>
2023-10-18 21:45:29 +0200

    FC

---
+++ YouTube/YouTube/YouTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTubeApp.swift
+//  YouTube
+//
+//  Created by Antoine Duval on 18/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct YouTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
